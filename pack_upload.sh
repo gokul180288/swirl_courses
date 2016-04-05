08fa2b4f28f484dc0e55cd0408e32918fa37805a
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-04-05 22:39:31 +0200

    add script to pack and upload

---
+++ pack_upload.sh
@@ -0,0 +1,7 @@
+# R_Programming
+zip -r R_Programming.zip R_Programming/*
+s3cmd put --acl-public R_Programming.zip s3://assets.datacamp.com/course/swirl_zip/
+
+# Getting_and_Cleaning_Data
+zip -r Getting_and_Cleaning_Data.zip Getting_and_Cleaning_Data/*
+s3cmd put --acl-public Getting_and_Cleaning_Data.zip s3://assets.datacamp.com/course/swirl_zip/

31208890b59b1cac5b1ae45a6c6dff1c85777430
westonstearns <weston@datacamp.com>
2016-04-15 17:28:37 -0400

    Update initLesson.R

--- Regression_Models/Binary_Outcomes/initLesson.R
+++ Regression_Models/Binary_Outcomes/initLesson.R
@@ -1,5 +1,5 @@
 # ravens data
-ravenData <- read.csv(file.path(find.package("swirl", quiet=TRUE), 
+ravenData <- read.csv(file.path("~", ".datacamp", 
                                  "Courses/Regression_Models/Binary_Outcomes/ravens_data.csv"))
 ravenData <- ravenData[order(ravenData$ravenScore), 1:3]
 rownames(ravenData) <- NULL

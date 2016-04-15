3426798fa7ceb25821e5a797d37531e457a3dc9f
westonstearns <weston@datacamp.com>
2016-04-15 17:30:03 -0400

    Update initLesson.R

--- Regression_Models/Introduction_to_Multivariable_Regression/initLesson.R
+++ Regression_Models/Introduction_to_Multivariable_Regression/initLesson.R
@@ -1,10 +1,10 @@
 local({
-  fname <- file.path(find.package("swirl"), 
+  fname <- file.path("~", ".datacamp", 
                      "Courses/Regression_Models/Introduction_to_Multivariable_Regression/elimination.R")
   file.copy(fname, "elimination.R")
   file.edit("elimination.R")
 })
 source("elimination.R")
-galton <- read.csv(file.path(find.package("swirl"), "Courses/Regression_Models/Introduction", "galton.csv"))
+galton <- read.csv("~", ".datacamp", "Courses/Regression_Models/Introduction", "galton.csv"))
 trees <- datasets::trees
 trees <- cbind(Constant=rep(1.0, nrow(trees)), trees)

2cfe59858fde0f6b52870767ae0c3bac3cf54484
westonstearns <weston@datacamp.com>
2016-04-15 17:28:57 -0400

    Update initLesson.R

--- Regression_Models/Count_Outcomes/initLesson.R
+++ Regression_Models/Count_Outcomes/initLesson.R
@@ -1,4 +1,4 @@
 # Put initialization code in this file.
-hits <- read.csv(file.path(find.package("swirl"),
+hits <- read.csv(file.path("~", ".datacamp",
                              "Courses/Regression_Models/Count_Outcomes/leekGroupData.csv"), as.is=TRUE)
 hits[,"date"] <- as.Date(hits[,"date"])

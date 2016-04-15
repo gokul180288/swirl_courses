0b032fdef37ebd1f5967a4b1e077eed7da18f479
westonstearns <weston@datacamp.com>
2016-04-15 17:29:36 -0400

    Update initLesson.R

--- Regression_Models/Introduction/initLesson.R
+++ Regression_Models/Introduction/initLesson.R
@@ -1,2 +1,2 @@
 # Put initialization code in this file.
-galton <- read.csv(file.path(find.package("swirl"), "Courses/Regression_Models/Introduction", "galton.csv"))
+galton <- read.csv(file.path("~", ".datacamp","Courses/Regression_Models/Introduction", "galton.csv"))

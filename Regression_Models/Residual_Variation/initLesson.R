54d38bc00ba5715fb225062d784ad79d53a882be
westonstearns <weston@datacamp.com>
2016-04-15 17:32:17 -0400

    Update initLesson.R

--- Regression_Models/Residual_Variation/initLesson.R
+++ Regression_Models/Residual_Variation/initLesson.R
@@ -1,2 +1,2 @@
-galton <- read.csv(file.path(find.package("swirl"), "Courses/Regression_Models/Introduction", "galton.csv"))
+galton <- read.csv("~", ".datacamp", "Courses/Regression_Models/Introduction", "galton.csv"))
 n <- 928

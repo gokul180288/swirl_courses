4717eeb65cae6bb49ab8965ff9383d984a459f87
westonstearns <weston@datacamp.com>
2016-04-15 17:32:38 -0400

    Update initLesson.R

--- Regression_Models/Residuals/initLesson.R
+++ Regression_Models/Residuals/initLesson.R
@@ -1,5 +1,5 @@
-galton <- read.csv(file.path(find.package("swirl"), "Courses/Regression_Models/Introduction", "galton.csv"))
+galton <- read.csv(file.path("~", ".datacamp", "Courses/Regression_Models/Introduction", "galton.csv"))
 est <- function(slope, intercept)intercept + slope*galton$parent
 sqe <- function(slope, intercept)sum( (est(slope, intercept)-galton$child)^2)
 attenu <- datasets::attenu
-fname <- paste(find.package("swirl"),"Courses/Regression_Models","Residuals","res_eqn.R",sep="/")
+fname <- paste("~", ".datacamp","Courses/Regression_Models","Residuals","res_eqn.R",sep="/")

798ccb2245f5792e09e971ab96277bda01d0f27c
Filip Schouwenaars <filip@datacamp.com>
2016-10-27 19:13:07 +0200

    update scirpt

--- pack_upload.sh
+++ pack_upload.sh
@@ -1,11 +1,11 @@
+# Make sure to install aws cli and set up a profile that has the appropriate access rights:
+folders=(R_Programming Getting_and_Cleaning_Data Exploratory_Data_Analysis Statistical_Inference)
 
-# zip -r R_Programming.zip R_Programming/*
-# zip -r Getting_and_Cleaning_Data.zip Getting_and_Cleaning_Data/*
-# zip -r Exploratory_Data_Analysis.zip Exploratory_Data_Analysis/*
-# zip -r Statistical_Inference.zip Statistical_Inference/*
-
-zip -r Regression_Models.zip Regression_Models/*
-aws s3 cp --acl public-read Regression_Models.zip s3://assets.datacamp.com/course/swirl/
+for folder in "${folders[@]}"
+do
+    zip -r $folder.zip $folder/*
+    aws s3 cp --acl public-read $folder.zip s3://assets.datacamp.com/course/swirl/
+done
 
 
 

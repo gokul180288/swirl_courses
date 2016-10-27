30cb2c2010cc1611975fc0fce0241ac2b3f97a3d
Filip Schouwenaars <filip@datacamp.com>
2016-10-27 17:25:33 +0200

    fixes

--- Regression_Models/Residuals/initLesson.R
+++ Regression_Models/Residuals/initLesson.R
@@ -1,6 +1,5 @@
 galton <- read.csv(file.path("~", ".datacamp", "Courses", "Regression_Models", "Introduction", "galton.csv"))
 est <- function(slope, intercept)intercept + slope*galton$parent
-sqe <- function(slope, intercept)sum( (est(slope, intercept)-galton$child)^2)
+sqe <- function(slope, intercept) sum( (est(slope, intercept)-galton$child)^2)
 attenu <- datasets::attenu
-fname <- paste("~", ".datacamp","Courses", "Regression_Models", "Residuals", "res_eqn.R", sep="/")
 
--- pack_upload.sh
+++ pack_upload.sh
@@ -1,7 +1,12 @@
 
-zip -r R_Programming.zip R_Programming/*
-zip -r Getting_and_Cleaning_Data.zip Getting_and_Cleaning_Data/*
-zip -r Exploratory_Data_Analysis.zip Exploratory_Data_Analysis/*
+# zip -r R_Programming.zip R_Programming/*
+# zip -r Getting_and_Cleaning_Data.zip Getting_and_Cleaning_Data/*
+# zip -r Exploratory_Data_Analysis.zip Exploratory_Data_Analysis/*
+# zip -r Statistical_Inference.zip Statistical_Inference/*
+
 zip -r Regression_Models.zip Regression_Models/*
-zip -r Statistical_Inference.zip Statistical_Inference/*
+aws s3 cp --acl public-read Regression_Models.zip s3://assets.datacamp.com/course/swirl/
+
+
+
 
